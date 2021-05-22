7b9797a332be6c70742ef77dd3af18a76a8586f8
Ang Jun Ray <angjunray@live.com>
2021-05-22 09:35:33 +0800

    first commit

---
+++ Jokes App/Jokes App/Jokes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_AppApp.swift
+//  Jokes App
+//
+//  Created by Ang Jun Ray on 22/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
